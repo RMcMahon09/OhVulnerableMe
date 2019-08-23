41b65e8775bd7ee2dd8730b17d64393a289694ba
RMcMahon09 <RMcMahon09@users.noreply.github.com>
2019-08-23 13:19:47 -0700

    Add files via upload

---
+++ blog-php/includes/footer.php
@@ -0,0 +1,7 @@
+		<div class="footer">
+			<p>Oh Vulnerable Me!&copy; <?php echo date('Y'); ?></p>
+		</div>
+	</div>
+	<!-- // container -->
+</body>
+</html>
---
+++ blog-php/includes/navbar.php
@@ -0,0 +1,12 @@
+<div class="navbar">
+	<div class="logo_div">
+		<a href="index.php"><h1>Oh Vulnerable Me!</h1></a>
+	</div>
+	<ul>
+	  <li><a class="active" href="index.php">Home</a></li>
+	  <li><a href="/blog-php/command.php">Connection</a></li>
+	  <li><a href="/blog-php/fileindex.php">Upload</a></li>
+	  <li><a href="#contact">Contact</a></li>
+	  <li><a href="#about">About</a></li>
+	</ul>
+</div>
