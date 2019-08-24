8ed78565c725fe270229242cdfe76f08b91d01af
RMcMahon09 <RMcMahon09@users.noreply.github.com>
2019-08-24 13:27:43 -0500

    Add files via upload

--- blog-php/admin/dashboard.php
+++ blog-php/admin/dashboard.php
@@ -21,15 +21,15 @@
 		<h1>Welcome</h1>
 		<div class="stats">
 			<a href="users.php" class="first">
-				<span>43</span> <br>
+				<br>
 				<span>Newly registered users</span>
 			</a>
 			<a href="posts.php">
-				<span>43</span> <br>
+				<br>
 				<span>Published posts</span>
 			</a>
 			<a>
-				<span>43</span> <br>
+				<br>
 				<span>Published comments</span>
 			</a>
 		</div>
